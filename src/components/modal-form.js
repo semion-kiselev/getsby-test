e3dfaf89c9eca8438d58ae5f3d4c61b5d2dab185
Siamion_Kisialiou <Siamion_Kisialiou@epam.com>
2018-12-01 16:53:25 +0300

    add form

---
+++ src/components/modal-form.js
@@ -0,0 +1,84 @@
+import React, {PureComponent} from 'react';
+import styled from 'styled-components';
+
+const StyledLabel = styled.span`
+    display: block;
+`;
+
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+}
+
+class ModalForm extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            firstName: '',
+            lastName: ''
+        }
+
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onInputChange(fieldName) {
+        return (e) => {
+            this.setState({
+                [fieldName]: e.target.value
+            });
+        }
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        // const formData = encode({ 'form-name': 'userFullName', ...this.state });
+        const formData = encode(this.state);
+        console.log(formData);
+
+        fetch('/', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: formData
+        })
+    }
+
+    render() {
+        const {firstName, lastName} = this.state;
+
+        return (
+            <form name="userFullName" method="post" data-netlify="true">
+                <p>
+                    <StyledLabel htmlFor="firstName">First Name:</StyledLabel>
+                    <input
+                        type="text"
+                        id="firstName"
+                        name="firstName"
+                        value={firstName}
+                        onChange={this.onInputChange('firstName')}
+                    />
+                </p>
+                <p>
+                    <StyledLabel htmlFor="lastName">Last Name:</StyledLabel>
+                    <input
+                        type="text"
+                        id="lastName"
+                        name="lastName"
+                        value={lastName}
+                        onChange={this.onInputChange('lastName')}
+                    />
+                </p>
+                <p>
+                    <button onClick={this.onSubmit}>
+                        Send
+                    </button>
+                </p>
+            </form>
+        );
+    }
+}
+
+export default ModalForm;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
+import Modal from 'react-modal';
 import Layout from '../components/layout';
+import ModalForm from '../components/modal-form';
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi, this is Home page</h1>
-    <p>Welcome Home :)</p>
-  </Layout>
-);
+class IndexPage extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            formIsVisible: false
+        }
+
+        this.showForm = this.showForm.bind(this);
+        this.closeForm = this.closeForm.bind(this);
+    }
+
+    showForm() {
+        this.setState({formIsVisible: true});
+    }
+
+    closeForm() {
+        this.setState({formIsVisible: false});
+    }
+
+    render() {
+        const {formIsVisible} = this.state;
+
+        return (
+            <Layout>
+                <h1>Hi, this is Home page</h1>
+                <p>Welcome Home :)</p>
+                <p>
+                    <button
+                        onClick={this.showForm}
+                    >
+                        Open Form in Modal
+                    </button>
+                </p>
+                <Modal
+                    isOpen={formIsVisible}
+                    onRequestClose={this.closeForm}
+                >
+                    <ModalForm />
+                </Modal>
+            </Layout>
+        );
+    }
+}
 
 export default IndexPage;
